10aec84abe7d712416ebf69afb1a96e7db83ac80
k1oShd <78645444+k1oShd@users.noreply.github.com>
2021-02-23 21:08:46 +0600

    Add files via upload

---
+++ lab4/src/app/products.ts
@@ -0,0 +1,125 @@
+export const products = [
+  {
+    id: 1,
+    name: "Seagate Portable 2TB External Hard Drive",
+    description:
+      "Portable HDD – USB 3.0 for PC, Mac, PS4, & Xbox - 1-Year Rescue Service (STGX2000400)",
+    rating: 4.5,
+    link:
+      "https://www.amazon.com/Seagate-Portable-External-Hard-Drive/dp/B07CRG94G3/ref=lp_16225007011_1_3",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81tjLksKixL._AC_SL1500_.jpg"
+  },
+  {
+    id: 2,
+    name: "Blue Yeti USB Mic",
+    description:
+      "Recording & Streaming on PC and Mac, 3 Condenser Capsules, 4 Pickup Patterns, Headphone Output and Volume Control, Mic Gain Control, Adjustable Stand, Plug & Play – Blackout",
+    rating: 4.5,
+    link:
+      "https://www.amazon.com/Blue-Yeti-USB-Microphone-Blackout/dp/B00N1YPXW2/ref=sr_1_23_mod_primary_new?dchild=1&qid=1614090296&s=computers-intl-ship&sbo=RZvfv%2F%2FHxDF%2BO5021pAnSA%3D%3D&sr=1-23",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61Ly9zgVyIL._AC_SL1500_.jpg"
+  },
+
+  {
+    id: 3,
+    name: "Webcam with Stereo Microphone and Privacy Cover, NexiGo N930AF",
+    description:
+      "FHD USB Web Camera, for Streaming Online Class, Compatible with Zoom/Skype/Facetime/Teams, PC Mac Laptop Desktop",
+    rating: 4.5,
+    link:
+      "https://www.amazon.com/AutoFocus-Microphone-NexiGo-Streaming-Compatible/dp/B08931JJLV/ref=sr_1_12?dchild=1&qid=1614090296&s=computers-intl-ship&sr=1-12",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61BGZKR0OBL._AC_SL1500_.jpg"
+  },
+
+  {
+    id: 4,
+    name: "Soundance Laptop Stand",
+    description:
+      "Aluminum Computer Riser, Ergonomic Laptops Elevator for Desk, Metal Holder Compatible with 10 to 15.6 Inches Notebook Computer, Silver",
+    rating: 5,
+    link:
+      "https://www.amazon.com/Ergonomic-Compatible-Notebook-Soundance-LS1/dp/B07D74DT3B/ref=sr_1_3_sspa?dchild=1&qid=1614090296&s=computers-intl-ship&sr=1-3-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExOU5YTzEzMkQ4QkRRJmVuY3J5cHRlZElkPUEwNzg4MzA0MlkwNzVNQzFGOVhSMiZlbmNyeXB0ZWRBZElkPUEwNzE1NDQ1MkE2OVZVUTYzMTBLWCZ3aWRnZXROYW1lPXNwX2F0Zl9icm93c2UmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/71Ox7ORXCsL._AC_SL1500_.jpg"
+  },
+
+  {
+    id: 5,
+    name: "AMD Ryzen 5 3600 6-Core",
+    description:
+      "2-Thread Unlocked Desktop Processor with Wraith Stealth Cooler",
+    rating: 5,
+    link:
+      "https://www.amazon.com/AMD-Ryzen-3600-12-Thread-Processor/dp/B07STGGQ18/ref=lp_16225007011_1_12",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/71WPGXQLcLL._AC_SL1384_.jpg"
+  },
+
+  {
+    id: 6,
+    name: "HP VH240a 23.8-Inch Full HD 1080p",
+    description:
+      "IPS LED Monitor with Built-In Speakers and VESA Mounting, Rotating Portrait & Landscape, Tilt, and HDMI & VGA Ports (1KL30AA) - Black",
+    rating: 4.7,
+    link: "2-Thread Unlocked Desktop Processor with Wraith Stealth Cooler",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/71trhuzbhML._AC_SL1500_.jpg"
+  },
+
+  {
+    id: 7,
+    name: "Roku Premiere",
+    description:
+      "HD/4K/HDR Streaming Media Player, Simple Remote and Premium HDMI Cable",
+    rating: 4.7,
+    link:
+      "https://www.amazon.com/Roku-Premiere-HDR-Streaming-Player-Premium/dp/B07HDBZN7Q/ref=lp_16225007011_1_9",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81wHpn6wPLL._AC_SL1500_.jpg"
+  },
+
+  {
+    id: 8,
+    name: "Acer SB220Q bi 21.5 Inches Full HD",
+    description:
+      "(1920 x 1080) IPS Ultra-Thin Zero Frame Monitor (HDMI & VGA Port), Black",
+    rating: 4.7,
+    link:
+      "https://www.amazon.com/Acer-SB220Q-Ultra-Thin-Frame-Monitor/dp/B07CVL2D2S/ref=lp_16225007011_1_5",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81QpkIctqPL._AC_SL1500_.jpg"
+  },
+
+  {
+    id: 9,
+    name: "TechOrbits Three Monitor Stand Mount",
+    description:
+      "SmartSWIVEL - Triple Computer Screen Desk Mount Arms - Full Motion Swivel Articulating Gas Springs - Universal Fit for 13 - 30 Screens Vesa Mount",
+    rating: 4.5,
+    link:
+      "https://www.amazon.com/TechOrbits-Three-Monitor-Stand-Mount/dp/B075SRGSK1/ref=sr_1_13_sspa?dchild=1&qid=1614091057&s=computers-intl-ship&sr=1-13-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExTk9RWFBTSjhYQlAxJmVuY3J5cHRlZElkPUEwOTIwMTczM1Q4MjlVNTEzUk1ZQyZlbmNyeXB0ZWRBZElkPUEwNjY5MjgzSVZONkYxME5OQ1BVJndpZGdldE5hbWU9c3BfbXRmX2Jyb3dzZSZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81t13XZdv6L._AC_SL1500_.jpg"
+  },
+
+  {
+    id: 10,
+    name: "FLYPROFiber",
+    description:
+      "10G SR MMF SFP+ to Fiber Transceiver for Intel, 10GBASE-SR for Intel E10GSFPSRX 850nm 300m DOM",
+    rating: 4.7,
+    link:
+      "https://www.amazon.com/FLYPROFiber-Fiber-Transceiver-10GBASE-SR-E10GSFPSRX/dp/B08FWTTF9V/ref=sr_1_3_sspa?dchild=1&fst=as%3Aoff&pf_rd_i=16225007011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=74069509-93ef-4a3c-8dca-a9e3fa773a64&pf_rd_r=96HN40HAEEZMCTHK9XM8&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1487012920&rnid=16225007011&s=computers-intl-ship&sr=1-3-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyNDU5UlQyOUQ2TkpSJmVuY3J5cHRlZElkPUEwNjM5ODAyU1NNUFFBWTU4OVozJmVuY3J5cHRlZEFkSWQ9QTA0MDk1NjJRNlRESkdVU05UWjgmd2lkZ2V0TmFtZT1zcF9hdGZfYnJvd3NlJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61T%2BBeL-epL._AC_SL1500_.jpg"
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
